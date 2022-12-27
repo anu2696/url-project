e3706946e80ea66216ac3180dc65147f3c8ad94d
Anushree <bhatnagar.anushree96@gmail.com>
2022-12-27 13:52:42 +0530

    Added auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { LongUrlComponent } from './long-url/long-url.component';
 import { ShortUrlComponent } from './short-url/short-url.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/url', pathMatch: 'full'},
   {path:'longURL', component:LongUrlComponent},
-  {path:'shortURL', component: ShortUrlComponent}
+  {path:'shortURL', component: ShortUrlComponent},
+  {path:'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShortUrlComponent } from './short-url/short-url.component';
 import { LongUrlComponent } from './long-url/long-url.component';
 import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShortUrlComponent,
     LongUrlComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
