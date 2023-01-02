012246ca3165717be9e707c87c9ed1b5d92d3791
Anushree <bhatnagar.anushree96@gmail.com>
2023-01-03 00:47:32 +0530

    Modified the Auth Page to make it UI friendly

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,30 @@
 import { HttpClient } from "@angular/common/http";
-import { Component } from "@angular/core";
+import { Component, Injectable } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
 
 @Component({
     selector: 'app-auth',
-    templateUrl: './auth.component.html'
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
+  })
+  @Injectable({
+    providedIn: 'root'
   })
 export class AuthComponent{
 
-  constructor(private http: HttpClient){}
+  public isLoggedIn = false;
+
+  constructor(private http: HttpClient, private route: Router){}
 
   onSignUp(form:NgForm){
     return this.http.post("http://localhost:8081/signUp", {
       "email": form.value.email,
       "password": form.value.password
     }).subscribe(response=> {
+      this.route.navigate(['longURL']);
+      this.isLoggedIn = true;
       console.log(response);
     });
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { AuthComponent } from '../auth/auth.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class HeaderComponent {
+
+
+
 
 }
